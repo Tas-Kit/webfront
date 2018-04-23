dcdbc083dbec3403d87d921a39340f8715d1f252
Lazy-Y <zhenyanz@usc.edu>
2018-04-23 16:27:16 -0700

    Remove userprofile

--- webfront/admin.py
+++ webfront/admin.py
@@ -4,7 +4,5 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from webfront.models import UserProfile
 # Register your models here.
-admin.site.register(UserProfile)
 admin.site.site_header = 'TasKit'
